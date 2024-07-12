8f851fa960904a8fd85c5906655a52a09178d4aa
vivaanchauhan1969 <141391553+vivaanchauhan1969@users.noreply.github.com>
2024-07-12 00:03:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,3 +27,41 @@ scissors = '''
 
 
 import random
+
+f = input("can you plz pick a choice 0 for rock , 1 for paper, 2 for scissors\n") 
+
+if f == "0":
+    print("your choice" + rock)
+    f = rock
+elif f == "1":
+    print("your choice"+ paper)
+    f = paper
+elif f == "2":
+    print("your choice"+ scissors)
+    f = scissors
+   
+
+List = [rock,paper,scissors]
+
+random_n = random.choice(List)
+
+print("computer pick" + random_n)
+
+if random_n == rock  and f == paper:
+    print("you win :)")
+elif random_n == rock and f == scissors:
+    print("you lose :(")
+elif random_n == rock and f == rock:
+    print(":P draw")
+elif random_n == paper and f == paper:
+    print(":P draw")
+elif random_n == paper and f == rock:
+     print("you lose :(")
+elif random_n == paper and f == scissors:
+     print("you win :)")
+elif random_n == scissors and f == rock:
+     print("you win :)")
+if random_n == scissors  and f == paper:
+     print("you lose :(")
+elif random_n == scissors and f == scissors:
+    print(":P draw")
